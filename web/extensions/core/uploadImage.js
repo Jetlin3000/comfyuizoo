042cdacf1a3535b47ce6c1d6ce605f998f5bbc29
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2023-08-10 13:53:09 +0800

    汉化+修正 uploadImage.js

--- web/extensions/core/uploadImage.js
+++ web/extensions/core/uploadImage.js
@@ -5,7 +5,7 @@ import { app } from "../../scripts/app.js";
 app.registerExtension({
 	name: "Comfy.UploadImage",
 	async beforeRegisterNodeDef(nodeType, nodeData, app) {
-		if (nodeData.name === "图像加载_Zho" || nodeData.name === "LoadImageMask") {
+		if (nodeData.name === "LoadImage" || nodeData.name === "图像加载_Zho" || nodeData.name === "LoadImageMask") {
 			nodeData.input.required.upload = ["IMAGEUPLOAD"];
 		}
 	},
